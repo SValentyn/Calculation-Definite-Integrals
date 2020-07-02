e0b49be18e8390d8e2aa1db13577dcaebd58d76b
SValentyn <planet.sv01@gmail.com>
2020-07-02 04:39:07 +0300

    Renamed MiddleRectangleMethod class to MidpointRectangleMethod class

--- src/MidpointRectangleMethod.java
+++ src/MidpointRectangleMethod.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class MiddleRectangleMethod {
+public class MidpointRectangleMethod {
 
     public final static int NUMBER_OF_PARTITIONS = 100;
 
